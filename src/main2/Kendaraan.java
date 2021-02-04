7fd0508a61ecbfd44f44939b95bab63b8ea215cd
Ziya-0221 <ziyaindi21@gmail.com>
2021-02-04 14:06:22 +0700

    Static Method Kendaraan

---
+++ src/main2/Kendaraan.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main2;
+
+/**
+ *
+ * @author indi
+ */
+public class Kendaraan {
+    
+    
+    
+    static void Spek(){
+        String tipe ="Opel Blazer";
+        String brand="Opel";
+        String Bakar="Solar";
+        String transmisi="manual 5 speed";
+        String daya="138 Hp/5.600 rpm";
+        String torsi="195 Nm / 3.400 rpm";
+        System.out.println("*****Mobil*****");
+        System.out.println("Type          : "+tipe);
+        System.out.println("Brand         : "+brand);
+        System.out.println("Bahan Bakar   : "+Bakar);
+        System.out.println("Transmisi     : "+transmisi);
+        System.out.println("Daya Maksimal : "+daya);
+        System.out.println("Torsi Maksimal: "+torsi);
+    }
+}
---
+++ src/main2/Main2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main2;
+
+/**
+ *
+ * @author indi
+ */
+public class Main2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Kendaraan.Spek();
+    }
+    
+}
